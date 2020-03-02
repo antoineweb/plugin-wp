168218f9d960785090fbe359ebe0353df89f9ed6
antoineweb <31164449+antoineweb@users.noreply.github.com>
2020-03-02 17:07:09 +0100

    ok

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Zero plugin
+Plugin URI: http://zero-plugin.com
+Description: Un plugin d'introduction pour le développement sous WordPress
+Version: 0.1
+Author: Midnight Falcon
+Author URI: http://votre-site.com
+License: GPL2
+*/?>
